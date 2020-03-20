331c7340ef91bca2bab68d497f5cfdd42755df5b
GavinWz <gavinwzmails@gmail.com>
2020-03-20 15:43:28 +0800

    update version

---
+++ classify-symbols.cc
@@ -0,0 +1,30 @@
+#include<unicode/ustdio.h>
+#include<unicode/ustring.h>
+#include<unicode/uchar.h>
+#include<iostream>
+#include<stdlib.h>
+
+int main(int argc, char **argv){
+	const int N = 100;
+
+	UChar *chars = (UChar*)malloc(N * sizeof(UChar));
+
+	UFILE *out = u_get_stdout();
+	UFILE *in = u_fadopt(stdin, NULL, NULL);
+	if(!in){
+		fputs("Couldn't get the stdandard input stream.\n", stderr);
+		return EXIT_FAILURE;
+	}
+	u_fgets(chars, N, in);
+	int len = u_strlen(chars);
+	for(int i = 0; i < len - 1; i++){
+		UCharCategory cat = (UCharCategory)u_charType(chars[i]);
+		if(cat == U_LOWERCASE_LETTER || cat == U_UPPERCASE_LETTER)
+			std::cout << "C ";
+		else
+			std::cout << "X ";
+		u_fputc(chars[i], out);
+ 	  	u_fputc('\n', out);
+	}
+	return EXIT_SUCCESS;
+}
